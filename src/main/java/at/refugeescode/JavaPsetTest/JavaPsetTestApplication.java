5ee8ba43a9a8d19afb8b92f27c02ec8a146292d1
unknown <engmohammmad1984@yahoo.com>
2018-03-21 15:56:32 +0100

    Added Spring boot project

---
+++ src/main/java/at/refugeescode/JavaPsetTest/JavaPsetTestApplication.java
@@ -0,0 +1,12 @@
+package at.refugeescode.JavaPsetTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaPsetTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaPsetTestApplication.class, args);
+	}
+}
---
+++ src/test/java/at/refugeescode/JavaPsetTest/JavaPsetTestApplicationTests.java
@@ -0,0 +1,16 @@
+package at.refugeescode.JavaPsetTest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JavaPsetTestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
